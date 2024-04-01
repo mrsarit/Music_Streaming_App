c27054aed3ca4191ecec4564c4df46689938455d
mrsarit <sarit.chanda@gmail.com>
2024-04-01 16:11:59 +0530

    3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 def create_app():
     app = Flask(__name__)
+    with app.app_context():
+        import application.views
     return app
 app = create_app()
 if __name__ == '__main__':
